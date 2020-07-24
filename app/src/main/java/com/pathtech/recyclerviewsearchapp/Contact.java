957371e78414552f8cbce14b6d3eb5f0e96c8714
Sharathkumar <mailtokgsharath@gmail.com>
2020-07-24 13:01:15 +0530

    Added Search bar and func

---
+++ app/src/main/java/com/pathtech/recyclerviewsearchapp/Contact.java
@@ -0,0 +1,26 @@
+package com.pathtech.recyclerviewsearchapp;
+
+/**
+ * Created by Sharath
+ */
+
+public class Contact {
+    String name;
+    String image;
+    String phone;
+
+    public Contact() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+}
